459d8388828362e63ba8e1befaa13391b4e64af6
ngxpaul <phongnx.22git@vku.udn.vn>
2024-08-22 15:29:14 +0700

    update navbar

--- src/component/AboutUs/index.tsx
+++ src/component/AboutUs/index.tsx
@@ -1,15 +1,6 @@
 import React from "react";
 
-import { Carousel } from "antd";
 
-const contentStyle: React.CSSProperties = {
-  margin: 0,
-  height: "160px",
-  color: "#fff",
-  lineHeight: "160px",
-  textAlign: "center",
-  background: "#364d79",
-};
 const AboutUs: React.FC = () => {
   return (
     <div className="pl-6">
