35495870461a7d29bca8001ed8918be15026e203
xuanphong230104 <nguyenxuanphong230104@gmail.com>
2024-08-29 16:16:54 +0700

    add img and fix a little bit

--- src/component/Card/index.tsx
+++ src/component/Card/index.tsx
@@ -1,21 +1,26 @@
-import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { IconProp } from '@fortawesome/fontawesome-svg-core';
-import './index.scss'; // Import your custom CSS for home styles
-
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { IconProp } from "@fortawesome/fontawesome-svg-core";
+import "./index.scss"; // Import your custom CSS for home styles
 const Card = ({ title, description, icon }) => {
   return (
     <div className="cardContainer">
-      <div className="card">
-        <div className="iconContainer">
-          <FontAwesomeIcon
-            icon={icon as IconProp}
-            className="icon" // Removed border and padding adjusted
-          />
+      <div  className="card">
+        <div className=" ">
+          <div className="iconContainer">
+            <FontAwesomeIcon
+              icon={icon as IconProp}
+              className="icon " // Removed border and padding adjusted
+            />
+          </div>
         </div>
         <div className="textContainer">
-          <h5 className="title">{title}</h5>
-          <p className="description">{description}</p>
+          <h5 className="title">
+            {title}
+          </h5>
+          <p className="description">
+            {description}
+          </p>
         </div>
       </div>
     </div>
